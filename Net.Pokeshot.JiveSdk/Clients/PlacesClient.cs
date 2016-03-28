778fbc457428a4d13d9f8ab68e61ca2fc9c5a9c3
Nathan Orick <cnathanorick@hotmail.com>
2016-03-28 17:05:37 -0400

    Add Space class

--- Net.Pokeshot.JiveSdk/Clients/PlacesClient.cs
+++ Net.Pokeshot.JiveSdk/Clients/PlacesClient.cs
@@ -4,11 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net;
+using Net.Pokeshot.JiveSdk.Models;
 
 namespace Net.Pokeshot.JiveSdk.Clients
 {
-    class PlacesClient : JiveClient
+    public class PlacesClient : JiveClient
     {
+        string placeUrl { get { return JiveCommunityUrl + "/api/core/v3/places"; } }
         public PlacesClient(string communityUrl, NetworkCredential credentials) : base(communityUrl, credentials) { }
+
+        //GetActivity()
+        //GetAppliedEntitlements()
+        //GetContent()
+        //GetExtProps()
+        //GetExtPropsForAddOn()
+        //GetFeaturedContent()
+        //GetPages()
+
+        JivePlace
     }
 }
