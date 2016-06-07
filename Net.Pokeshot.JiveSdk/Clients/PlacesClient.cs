2764f7c0481dd522044f3d1eba4f1685d4294a4a
doublerazr <tsharpe1@vols.utk.edu>
2016-06-07 15:08:24 -0400

    Added implementation for creating contents in and getting contents from a Place

--- Net.Pokeshot.JiveSdk/Clients/PlacesClient.cs
+++ Net.Pokeshot.JiveSdk/Clients/PlacesClient.cs
@@ -17,6 +17,76 @@ namespace Net.Pokeshot.JiveSdk.Clients
         public PlacesClient(string communityUrl, NetworkCredential credentials) : base(communityUrl, credentials) { }
 
         /// <summary>
+        /// Create a new content object with specified characteristics, and return an entity representing the newly created content object.
+        /// </summary>
+        /// <param name="placeID">ID of the place the content should be added to</param>
+        /// <param name="content">a GenericContent object describing the content object to be created</param>
+        /// <param name="published">a DateTime of when this content object was originally created. Set 'updated' param as well. Only set this field when importing content.</param>
+        /// <param name="updated">a DateTime of when this content object was most recently updated. Set 'published' param as well. Only set this field when importing content.</param>
+        /// <param name="fields">The fields to include in the returned entity</param>
+        /// <returns>a GenericContent object representing the newly created content</returns>
+        public GenericContent CreateContent(int placeID, GenericContent content, DateTime? published = null, DateTime? updated = null, List<string> fields = null)
+        {
+            //constructs the url for the HTTP request based on the user specifications
+            bool first = true;
+            string url = placesUrl + "/" + placeID.ToString() + "/contents";
+            if (published != null)
+            {
+                url += "?published=" + jiveDateFormat((DateTime)published);
+                first = false;
+            }
+            if (updated != null)
+            {
+                if (first) url += "?updated=";
+                else url += "&updated=";
+                url += jiveDateFormat((DateTime)updated);
+                first = false;
+            }
+            if (fields != null && fields.Count > 0)
+            {
+                if (first == true)
+                {
+                    url += "?fields=";
+                    first = false;
+                }
+                else url += "&fields=";
+
+                foreach (var field in fields)
+                {
+                    url += field + ",";
+                }
+                //remove last comma
+                url = url.Remove(url.Length - 1);
+            }
+
+            //converts the content to be added into JSON format and makes the HTTP request
+            string json = JsonConvert.SerializeObject(content, new JsonSerializerSettings { DefaultValueHandling = DefaultValueHandling.Ignore, Formatting = Formatting.Indented });
+            string result;
+            try
+            {
+                result = PostAbsolute(url, json);
+            }
+            catch (HttpException e)
+            {
+                switch (e.GetHttpCode())
+                {
+                    case 400:
+                        throw new HttpException(e.WebEventCode, "An input field is malformed");
+                    case 403:
+                        throw new HttpException(e.WebEventCode, "You are not allowed to access the specified content or place");
+                    case 409:
+                        throw new HttpException(e.WebEventCode, "The new entity would conflict with system restrictions (such as two contents of the same type with the same name");
+                    default:
+                        throw;
+                }
+            }
+
+            //converts the returned JSON into a GenericContent object and returns it to the user
+            JObject Json = JObject.Parse(result);
+            return Json.ToObject<GenericContent>();
+        }
+
+        /// <summary>
         /// Create a new category for a place with specified characteristics, and return an entity representing the newly created category.
         /// </summary>
         /// <param name="placeID">ID of the place for which to create a category</param>
@@ -100,7 +170,92 @@ namespace Net.Pokeshot.JiveSdk.Clients
 
         //GetActivity()
         //GetAppliedEntitlements()
-        //GetContent()
+
+        /// <summary>
+        /// Return a list of featured content objects in the specified place.
+        /// </summary>
+        /// <param name="placeID">ID of the place the objects are being retrieved from</param>
+        /// <param name="sort">Requested sort order.
+        /// Possible strings:
+        /// dateCreatedAsc - Sort by the date this place was created, in ascending order.
+        /// dateCreatedDesc - Sort by the date this place was created, in descending order. Default if none was specified.
+        /// latestActivityAsc - Sort by the date this place had the most recent activity, in ascending order.
+        /// latestActivityDesc - Sort by the date this place had the most recent activity, in descending order.
+        /// titleAsc - Sort by place name, in ascending order.</param>
+        /// <param name="type">one or more object types of desired contained content objects (document, discussion, post, poll) separated by commas</param>
+        /// <param name="fields">The fields to be returned on each content</param>
+        /// <param name="startIndex">The zero-relative index of the first matching content to be returned</param>
+        /// <param name="count">The maximum number of contents to be returned per Jive HTTP request</param>
+        /// <param name="abridged">Flag indicating that if content.text is requested, it will be abridged (length shortened, HTML tags removed)</param>
+        /// <returns>a GenericContent object list of the matched content objects</returns>
+        public List<GenericContent> GetContents(int placeID, string sort = "dateCreatedDesc", List<string> type = null, List<string> fields = null, int startIndex = 0, int count = 25, bool abridged = false)
+        {
+            //construct url for the HTTP request with the user specifications
+            string url = placesUrl + "/" + placeID.ToString() + "/contents";
+            url += "?sort=" + sort;
+            url += "&abridged=" + abridged.ToString();
+            url += "&count=" + count.ToString();
+            if (startIndex != 0) url += "&startIndex=" + startIndex.ToString();
+            if (type != null && type.Count > 0)
+            {
+                url += "&filter=type(";
+                foreach (var item in type)
+                {
+                    url += item + ",";
+                }
+                //remove last comma
+                url.Remove(url.Length - 1);
+
+                url += ")";
+            }
+            if (fields != null && fields.Count > 0)
+            {
+                url += "&fields=";
+                foreach (var field in fields)
+                {
+                    url += field + ",";
+                }
+                // remove last comma
+                url = url.Remove(url.Length - 1);
+            }
+
+            // jive returns a paginated list, so we have to loop through all of the pages.
+            string json;
+            JObject results;
+            List<GenericContent> contentList = new List<GenericContent>();
+            while (true)
+            {
+                try
+                {
+                    json = GetAbsolute(url);
+                }
+                catch (HttpException e)
+                {
+                    switch (e.GetHttpCode())
+                    {
+                        case 400:
+                            throw new HttpException(e.WebEventCode, "An input field is malformed");
+                        case 403:
+                            throw new HttpException(e.WebEventCode, "You are not allowed to access the specified content object or place");
+                        case 404:
+                            throw new HttpException(e.WebEventCode, "The specified place does not exist");
+                        default:
+                            throw;
+                    }
+                }
+                results = JObject.Parse(json);
+
+                contentList.AddRange(results["list"].ToObject<List<GenericContent>>());
+
+                if (results["links"] == null || results["links"]["next"] == null)
+                    break;
+                else
+                    url = results["links"]["next"].ToString();
+            }
+            return contentList;
+
+        }
+
         //GetExtProps()
         //GetExtPropsForAddOn()
         //GetFeaturedContent()
