1e77b248b7d7ed520a920bf25a7d4365ab6ea991
cnorick <cnathanorick@gmail.com>
2016-06-16 13:16:38 -0400

    Implement function to get activities from des

--- Net.Pokeshot.JiveSdk/Clients/DESClient.cs
+++ Net.Pokeshot.JiveSdk/Clients/DESClient.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using System.Net;
+using Net.Pokeshot.JiveSdk.Models.Dto;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
+using System;
+using System.Web;
+using Newtonsoft.Json.Linq;
 
 namespace Net.Pokeshot.JiveSdk.Clients
 {
@@ -18,10 +18,69 @@ namespace Net.Pokeshot.JiveSdk.Clients
             _clientSecret = clientSecret;
         }
 
-        public void test()
+        public List<JiveDEAActivityInstance> GetActivity(List<string> filter = null, int count = 100, List<string> fields = null, DateTime? before = null, DateTime? after = null,
+            bool showAll = false)
         {
-            getAuthorization();
+            List<JiveDEAActivityInstance> activityList = new List<JiveDEAActivityInstance>();
+
+            string url = _desUrl + "/activity";
+            url += "?count=" + (count > 1000000000 ? 1000000000 : count).ToString();
+            url += "&show-all=" + showAll;
+            if (after != null)
+                url += "&after=" + jiveDateFormat(after.Value);
+            if (before != null)
+                url += "&before=" + jiveDateFormat(before.Value);
+            if (fields != null && fields.Count > 0)
+            {
+                url += "&fields=";
+                foreach (var field in fields)
+                {
+                    url += field + ",";
+                }
+                // remove last comma
+                url = url.Remove(url.Length - 1);
+            }
+            if (filter != null && filter.Count > 0)
+            {
+                foreach (var item in filter)
+                {
+                    url += "&filter=" + item;
+                }
+            }
+
+            // jive returns a paginated list, so we have to loop through all of the pages.
+            while (true)
+            {
+                string json;
+                try
+                {
+                    json = GetAbsolute(url, getAuthorization());
+                }
+                catch (HttpException e)
+                {
+                    switch (e.GetHttpCode())
+                    {
+                        case 400:
+                            throw new HttpException(e.WebEventCode, "An input field is missing or malformed");
+                        case 403:
+                            throw new HttpException(e.WebEventCode, "You are not allowed to access this");
+                        default:
+                            throw;
+                    }
+                }
+                JObject results = JObject.Parse(json);
+
+
+                activityList.AddRange(results["list"].ToObject<List<JiveDEAActivityInstance>>());
+
+                if (results["paging"] == null || results["paging"]["next"] == null)
+                    break;
+                else
+                    url = results["paging"]["next"].ToString();
+            }
+            return activityList;
         }
+
 
         /// <summary>
         /// Gets the Authorization needed for downloading data from the DES.
