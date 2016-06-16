f283dff97140805e5f0c17dd02b7718f41934772
cnorick <cnathanorick@gmail.com>
2016-06-16 10:56:36 -0400

    Add another GetAbsolute that takes authorization string
    This is for use by the DES client. The other
    clients all use basic authentication with the
    username and password provided in the
    _credential. However, the DES client will use
    authentication fetched from the web.

--- Net.Pokeshot.JiveSdk/Clients/JiveClient.cs
+++ Net.Pokeshot.JiveSdk/Clients/JiveClient.cs
@@ -23,25 +23,34 @@ namespace Net.Pokeshot.JiveSdk.Clients
             _credential = cred;
         }
 
+
         protected string GetAbsolute(string url)
+        {      
+            //Getting our credentials in Base64 encoded format  
+            string creds = String.Format("{0}:{1}", _credential.UserName, _credential.Password);
+            byte[] bytes = Encoding.UTF8.GetBytes(creds);
+            creds = "Basic " + Convert.ToBase64String(bytes);
+
+            return GetAbsolute(url, creds);
+        }
+
+        protected string GetAbsolute(string url, string authorization)
         {
             HttpClientHandler jiveHandler = new HttpClientHandler();
 
             //Setting credentials for our request. This needs to be done for every request as there are no persistent sessions for the REST Api  
             _credential.Domain = JiveCommunityUrl + "/api/core/v3";
-            //Getting our credentials in Base64 encoded format  
-            string cre = String.Format("{0}:{1}", _credential.UserName, _credential.Password);
-            byte[] bytes = Encoding.UTF8.GetBytes(cre);
-            string base64 = Convert.ToBase64String(bytes);
+
             //Set credentials and make sure we are pre-authenticating our request  
             jiveHandler.Credentials = _credential;
             jiveHandler.PreAuthenticate = true;
             jiveHandler.UseDefaultCredentials = true;
 
+
             HttpClient httpClient = new HttpClient(jiveHandler);
-            httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", base64);
 
             HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Get, url);
+            requestMessage.Headers.Add("Authorization", authorization);
 
             HttpResponseMessage activityResponse = httpClient.SendAsync(requestMessage).Result;
 
@@ -57,7 +66,6 @@ namespace Net.Pokeshot.JiveSdk.Clients
             String myActivityResponse = activityResponse.Content.ReadAsStringAsync().Result;
             //Remove the string Jive includes in every response from the REST API  
             string cleanResponseActivities = myActivityResponse.Replace("throw 'allowIllegalResourceCall is false.';", "");
-
 
             return cleanResponseActivities;
         }
