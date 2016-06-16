df559a421609953081b4a446d23269834ddc2026
cnorick <cnathanorick@gmail.com>
2016-06-16 10:09:25 -0400

    Add DESClient

---
+++ Net.Pokeshot.JiveSdk/Clients/DESClient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net.Pokeshot.JiveSdk.Clients
+{
+    public class DESClient : JiveClient
+    {
+        string contentUrl { get { return "https://api.jivesoftware.com/analytics/v2/export"; } }
+        string _clientId;
+        string _clientSecret;
+
+        public DESClient(string communityUrl, NetworkCredential credentials, string clientId, string clientSecret) : base(communityUrl, credentials) {
+            _clientId = clientId;
+            _clientSecret = clientSecret;
+        }
+
+    }
+}
