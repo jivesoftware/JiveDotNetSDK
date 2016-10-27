6b65702a4c29e6e46a6d55820233c9f40ca5a090
Nathan Orick <cnathanorick@hotmail.com>
2016-10-27 16:56:14 -0400

    Add Get Tags search function

---
+++ Net.Pokeshot.JiveSdk/Models/ContentTag.cs
@@ -0,0 +1,12 @@
+﻿namespace Net.Pokeshot.JiveSdk.Models
+{
+    public class ContentTag
+    {
+        public string id { get; set; }
+        public string index { get; set; }
+        public string name { get; set; }
+        public Resources.Resources resources { get; set; }
+        public string type { get; set; }
+        public string uuid { get; set; }
+    }
+}
